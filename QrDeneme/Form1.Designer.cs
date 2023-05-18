0493100f55ed8895824a9f5acc559cdaac15d930
Alpay Akçer <info@alpayakcer.com>
2023-05-18 10:01:31 +0300

    Add project files.

---
+++ QrDeneme/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace QrDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grpBoxBilgiler = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtWeb = new System.Windows.Forms.TextBox();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblDateTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCopyrightYear = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblFirmaBilgi = new System.Windows.Forms.Label();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUnvan = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVersionNumarasi = new System.Windows.Forms.ToolStripStatusLabel();
+            this.grpBoxBilgiler.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBoxBilgiler
+            // 
+            this.grpBoxBilgiler.Controls.Add(this.label3);
+            this.grpBoxBilgiler.Controls.Add(this.label6);
+            this.grpBoxBilgiler.Controls.Add(this.label8);
+            this.grpBoxBilgiler.Controls.Add(this.label7);
+            this.grpBoxBilgiler.Controls.Add(this.label5);
+            this.grpBoxBilgiler.Controls.Add(this.label4);
+            this.grpBoxBilgiler.Controls.Add(this.label9);
+            this.grpBoxBilgiler.Controls.Add(this.label2);
+            this.grpBoxBilgiler.Controls.Add(this.label1);
+            this.grpBoxBilgiler.Controls.Add(this.txtCompany);
+            this.grpBoxBilgiler.Controls.Add(this.txtAdres);
+            this.grpBoxBilgiler.Controls.Add(this.txtWeb);
+            this.grpBoxBilgiler.Controls.Add(this.txtMobile);
+            this.grpBoxBilgiler.Controls.Add(this.txtTelefon);
+            this.grpBoxBilgiler.Controls.Add(this.txtMail);
+            this.grpBoxBilgiler.Controls.Add(this.txtSurname);
+            this.grpBoxBilgiler.Controls.Add(this.txtUnvan);
+            this.grpBoxBilgiler.Controls.Add(this.txtName);
+            resources.ApplyResources(this.grpBoxBilgiler, "grpBoxBilgiler");
+            this.grpBoxBilgiler.Name = "grpBoxBilgiler";
+            this.grpBoxBilgiler.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Name = "label3";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Name = "label6";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Name = "label7";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Name = "label4";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Name = "label1";
+            // 
+            // txtCompany
+            // 
+            resources.ApplyResources(this.txtCompany, "txtCompany");
+            this.txtCompany.Name = "txtCompany";
+            // 
+            // txtAdres
+            // 
+            resources.ApplyResources(this.txtAdres, "txtAdres");
+            this.txtAdres.Name = "txtAdres";
+            // 
+            // txtWeb
+            // 
+            resources.ApplyResources(this.txtWeb, "txtWeb");
+            this.txtWeb.Name = "txtWeb";
+            // 
+            // txtMobile
+            // 
+            resources.ApplyResources(this.txtMobile, "txtMobile");
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMobile_KeyPress);
+            // 
+            // txtTelefon
+            // 
+            resources.ApplyResources(this.txtTelefon, "txtTelefon");
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefon_KeyPress);
+            // 
+            // txtMail
+            // 
+            resources.ApplyResources(this.txtMail, "txtMail");
+            this.txtMail.Name = "txtMail";
+            // 
+            // txtSurname
+            // 
+            resources.ApplyResources(this.txtSurname, "txtSurname");
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSurname_KeyPress);
+            // 
+            // txtName
+            // 
+            resources.ApplyResources(this.txtName, "txtName");
+            this.txtName.Name = "txtName";
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblVersionNumarasi,
+            this.lblDateTime,
+            this.toolStripStatusLabel1,
+            this.lblCopyrightYear,
+            this.toolStripStatusLabel2});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            // 
+            // lblDateTime
+            // 
+            this.lblDateTime.Name = "lblDateTime";
+            resources.ApplyResources(this.lblDateTime, "lblDateTime");
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+            // 
+            // lblCopyrightYear
+            // 
+            this.lblCopyrightYear.Name = "lblCopyrightYear";
+            resources.ApplyResources(this.lblCopyrightYear, "lblCopyrightYear");
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            resources.ApplyResources(this.toolStripStatusLabel2, "toolStripStatusLabel2");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGenerate);
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblAdres);
+            this.groupBox3.Controls.Add(this.lblEmail);
+            this.groupBox3.Controls.Add(this.lblFirmaBilgi);
+            this.groupBox3.Controls.Add(this.lblAdSoyad);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // lblAdres
+            // 
+            resources.ApplyResources(this.lblAdres, "lblAdres");
+            this.lblAdres.Name = "lblAdres";
+            // 
+            // lblEmail
+            // 
+            resources.ApplyResources(this.lblEmail, "lblEmail");
+            this.lblEmail.Name = "lblEmail";
+            // 
+            // lblFirmaBilgi
+            // 
+            resources.ApplyResources(this.lblFirmaBilgi, "lblFirmaBilgi");
+            this.lblFirmaBilgi.Name = "lblFirmaBilgi";
+            // 
+            // lblAdSoyad
+            // 
+            resources.ApplyResources(this.lblAdSoyad, "lblAdSoyad");
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Name = "label9";
+            // 
+            // txtUnvan
+            // 
+            resources.ApplyResources(this.txtUnvan, "txtUnvan");
+            this.txtUnvan.Name = "txtUnvan";
+            this.txtUnvan.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnGenerate.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnGenerate, "btnGenerate");
+            this.btnGenerate.Image = global::QrDeneme.Properties.Resources.Iconsmind_Outline_Add_User_16;
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnKaydet.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnKaydet, "btnKaydet");
+            this.btnKaydet.ForeColor = System.Drawing.Color.Black;
+            this.btnKaydet.Image = global::QrDeneme.Properties.Resources.Pictogrammers_Material_Content_save_check_outline_24;
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVersionNumarasi
+            // 
+            this.lblVersionNumarasi.Name = "lblVersionNumarasi";
+            resources.ApplyResources(this.lblVersionNumarasi, "lblVersionNumarasi");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grpBoxBilgiler);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.grpBoxBilgiler.ResumeLayout(false);
+            this.grpBoxBilgiler.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxBilgiler;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtWeb;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDateTime;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCopyrightYear;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblFirmaBilgi;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUnvan;
+        private System.Windows.Forms.ToolStripStatusLabel lblVersionNumarasi;
+    }
+}
+
---
+++ QrDeneme/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace QrDeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            lblCopyrightYear.Text = DateTime.Now.Year.ToString();
+            SetDinamikVersiyon();
+        }
+
+        void SetDinamikVersiyon()
+        {
+            //Öncelikle AssemblyInfo.cs içindeki kısmı [assembly: AssemblyVersion(“1.0.*”)] şeklinde değiştirin
+
+            //string name = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            DateTime buildDate = new DateTime(2023, 1, 1).AddDays(version.Build).AddSeconds(version.Revision * 2);
+            string displayableVersion = version.ToString();
+            lblVersionNumarasi.Text = version.ToString() + " " + "|";
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Regex validate_emailaddress = email_validation();
+            try
+            {
+                if (txtName.Text != "" && txtSurname.Text != "")
+                {
+                    if (validate_emailaddress.IsMatch(txtMail.Text) != true)
+                    {
+                        MessageBox.Show("Geçersiz Mail Adresi Girdiniz!", "Geçersiz", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        txtMail.Focus();
+                        return;
+                    }
+
+                    var options = new QrCodeEncodingOptions
+                    {
+                        Height = pictureBox1.Height,
+                        Width = pictureBox1.Width
+                    };
+                    //create instance of BarcodeWrider
+                    var writer = new BarcodeWriter();
+                    //set format
+                    writer.Format = BarcodeFormat.QR_CODE;
+                    writer.Options = options;
+                    var text = $"BEGIN:VCARD\n" +
+                    $"VERSION: 3.0\n" +
+                        $"N: {txtName.Text} {txtSurname.Text};\n" +
+                        $"ORG:{txtCompany.Text}\n" +
+                        $"TITLE:{txtUnvan.Text}\n" +
+                        $"URL; WORK: {txtWeb.Text}\n" +
+                        $"TEL: {txtTelefon.Text}\n" +
+                        $"TEL: {txtMobile.Text}\n" +
+                        $"EMAIL; INTERNET: {txtMail.Text}\n" +
+                        $"ADR; INTL; PARCEL; WORK; CHARSET = utf - 8:; ; {txtAdres.Text};\n" +
+                        $"END:VCARD";
+                    var result = writer.Write(text);
+                    pictureBox1.Image = result;
+
+                    lblAdSoyad.Text = txtName.Text + " " + txtSurname.Text;
+                    lblFirmaBilgi.Text = txtCompany.Text;
+                    lblEmail.Text = txtMail.Text;
+                    lblAdres.Text = txtAdres.Text;
+
+                }
+                else
+                {
+                    MessageBox.Show("Yetkili Adınız ve Soyadını Boş Bırakmayınız");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata" + ex.Message);
+            }
+
+        }
+
+
+        private Regex email_validation()
+        {
+            string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(pattern, RegexOptions.IgnoreCase);
+        }
+
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string initialDIR = @"C:\Users\Maverick\Desktop\QRfiles";
+            var dialog = new SaveFileDialog();
+            dialog.InitialDirectory = initialDIR;
+            dialog.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(dialog.FileName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDateTime.Text = DateTime.Now.ToString();
+        }
+
+        private void txtTelefon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtMobile_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtSurname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                && !char.IsSeparator(e.KeyChar);
+        }
+    }
+}
